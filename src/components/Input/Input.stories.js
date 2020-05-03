76caa69db905810dfe867b563c6721406b82fc8c
Berk Sevgi <berksevgi95@hotmail.com>
2020-05-03 16:00:36 +0300

    v1.0.6 released

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,9 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Input from './Input';
+import { BSTheme } from '../../utils';
 
 export default {
   title: 'Input',
 };
 
-export const Default = () => <Input />;
+export const Default = () => <Input>Input</Input>;
+
+export const SecondaryTheme = () => <Input theme={BSTheme.SECONDARY}>Input</Input>;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,30 @@
+// /* eslint-disable react/jsx-props-no-spreading */
+// import React from 'react';
+// import { IComponentProps } from '../../utils';
+
+// import '../../index.scss';
+// import './Input.scss';
+
+// export interface IInputProps
+//   extends React.ComponentProps<'input'>, IComponentProps {
+// }
+
+// const Input: React.FC<IInputProps> = ({
+//   className,
+//   ...props
+// }) => (
+//   <input
+//     className="bs bs-input"
+//     {...props}
+//   />
+// );
+
+// export default Input;
+
+
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+
 import { IComponentProps } from '../../utils';
 
 import '../../index.scss';
@@ -11,12 +36,26 @@ export interface IInputProps
 
 const Input: React.FC<IInputProps> = ({
   className,
+  id,
+  name,
+  children,
+  theme,
   ...props
 }) => (
-  <input
-    className="bs bs-input"
-    {...props}
-  />
+  <label className={`bs bs-input ${className || ''} ${theme}`} htmlFor={id || 'text'}>
+    {children && (
+      <div>
+        {children}
+      </div>
+    )}
+    <input
+      id={id || 'text'}
+      type="text"
+      name={name}
+      className="bs"
+      {...props}
+    />
+  </label>
 );
 
 export default Input;
