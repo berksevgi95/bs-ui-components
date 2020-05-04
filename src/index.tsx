4b936836a58e2b719bf9e919a7839017a3638fca
Berk Sevgi <berksevgi95@hotmail.com>
2020-05-04 13:38:49 +0300

    v1.0.9 released

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,6 @@ export { default as Input } from './components/Input/Input';
 export { default as Checkbox } from './components/Checkbox/Checkbox';
 export { default as Chip } from './components/Chip/Chip';
 export { default as Dialog } from './components/Dialog/Dialog';
-export { default as Message } from './components/Message/Message';
+export { default as Message, EMessageTypes, EMessagePosition } from './components/Message/Message';
 export { default as Radiobutton } from './components/Radiobutton/Radiobutton';
 export { default as Select } from './components/Select/Select';
